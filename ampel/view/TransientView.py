61a91542448bd783b9af0679ff534680a1cfec3e
vbrinnel <valery.brinnel@gmail.com>
2020-05-29 17:06:55 +0200

    New class: TransientView

---
+++ ampel/view/TransientView.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File              : ampel/view/TransientView.py
+# License           : BSD-3-Clause
+# Author            : vb <vbrinnel@physik.hu-berlin.de>
+# Date              : 13.01.2018
+# Last Modified Date: 19.12.2019
+# Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
+
+from typing import Optional, List
+from dataclasses import dataclass
+from ampel.content.DataPoint import DataPoint
+from ampel.view.LightCurve import LightCurve
+from ampel.view.SnapView import SnapView
+
+@dataclass(frozen=True)
+class TransientView(SnapView):
+
+	def get_photopoints(self) -> Optional[List[DataPoint]]:
+
+		if not self.t0:
+			return None
+
+		# By convention photopoints have positive int ids
+		return [dp for dp in self.t0 if dp['_id'] > 0]
+
+
+	def get_upperlimits(self) -> Optional[List[DataPoint]]:
+
+		if not self.t0:
+			return None
+
+		# By convention photopoints have negative int ids
+		return [dp for dp in self.t0 if dp['_id'] < 0]
+
+
+	def get_lightcurves(self) -> Optional[List[LightCurve]]:
+
+		if not self.extra:
+			return None
+
+		return self.extra.get('lighcurve')
