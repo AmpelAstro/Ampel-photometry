63786a932cd79f96c5fcfa4e0872b9b4100b02d8
vbrinnel <valery.brinnel@gmail.com>
2020-05-29 17:02:49 +0200

    Add setup files

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File              : Ampel-photometry/setup.py
+# License           : BSD-3-Clause
+# Author            : vb <vbrinnel@physik.hu-berlin.de>
+# Date              : 11.12.2019
+# Last Modified Date: 28.01.2020
+# Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
+
+from setuptools import setup, find_namespace_packages
+
+setup(
+	name='ampel-photometry',
+	version='0.7',
+	packages=find_namespace_packages(),
+	package_data = {
+		'': ['py.typed'],
+		'conf': [
+			'*.json', '**/*.json', '**/**/*.json',
+			'*.yaml', '**/*.yaml', '**/**/*.yaml',
+			'*.yml', '**/*.yml', '**/**/*.yml'
+		]
+	}
+)
