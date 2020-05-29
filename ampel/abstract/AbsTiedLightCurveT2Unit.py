5945e549e0798cd30c2700cf29a3905133fbf288
vbrinnel <valery.brinnel@gmail.com>
2020-05-29 17:03:12 +0200

    New abstract class: AbsTiedLightCurveT2Unit

---
+++ ampel/abstract/AbsTiedLightCurveT2Unit.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File              : Ampel-photometry/ampel/abstract/AbsTiedLightCurveT2Unit.py
+# License           : BSD-3-Clause
+# Author            : vb <vbrinnel@physik.hu-berlin.de>
+# Date              : 11.03.2020
+# Last Modified Date: 03.04.2020
+# Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
+
+from typing import Sequence
+from ampel.base import abstractmethod
+from ampel.view.LightCurve import LightCurve
+from ampel.t2.T2UnitResult import T2UnitResult
+from ampel.content.T2Record import T2Record
+from ampel.abstract.AbsTiedCustomStateT2Unit import AbsTiedCustomStateT2Unit
+
+
+class AbsTiedLightCurveT2Unit(AbsTiedCustomStateT2Unit[LightCurve], abstract=True):
+
+	@abstractmethod
+	def run(self, # type: ignore
+		light_curve: LightCurve,
+		t2_records: Sequence[T2Record]
+	) -> T2UnitResult:
+		"""
+		Returned T2UnitResult dataclass should contain computed science results to be saved into the DB.
+		Notes: dict must have only string keys and values must be bson encodable
+		"""
