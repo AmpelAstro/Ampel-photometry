447a7547b159567d7b75b059bd4b2d1777bf8ae5
vbrinnel <valery.brinnel@gmail.com>
2020-08-07 14:51:59 +0200

    New class: DemoLightCurveT2Unit

---
+++ ampel/demo/unit/base/DemoLightCurveT2Unit.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File              : Ampel-photometry/ampel/demo/unit/base/DemoLightCurveT2Unit.py
+# License           : BSD-3-Clause
+# Author            : vb <vbrinnel@physik.hu-berlin.de>
+# Date              : 07.08.2020
+# Last Modified Date: 07.08.2020
+# Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
+
+from time import time
+from ampel.type import T2UnitResult
+from ampel.abstract.AbsLightCurveT2Unit import AbsLightCurveT2Unit
+from ampel.view.LightCurve import LightCurve
+
+
+class DemoLightCurveT2Unit(AbsLightCurveT2Unit):
+
+	test_parameter: int = 0
+
+	def run(self, lightcurve: LightCurve) -> T2UnitResult:
+		return {
+			"id": lightcurve.get_values("jd"),
+			"time": time(),
+			"test_parameter": self.test_parameter
+		}
