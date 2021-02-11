5b14404a250f11b571bf0ce8270b38d40cd46b65
vbrinnel <valery.brinnel@gmail.com>
2021-02-11 21:37:22 +0100

    Rename: PhotoT2Record -> PhotoT2Document

--- ampel/content/PhotoT2Document.py
+++ ampel/content/PhotoT2Document.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# File              : Ampel-photometry/ampel/content/PhotoT2Record.py
+# File              : Ampel-photometry/ampel/content/PhotoT2Document.py
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 09.02.2021
 # Last Modified Date: 09.02.2021
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
-from ampel.content.T2Record import T2Record
+from ampel.content.T2Document import T2Document
 
-class PhotoT2Record(T2Record, total=False):
+class PhotoT2Document(T2Document, total=False):
 	ppsid: bytes
