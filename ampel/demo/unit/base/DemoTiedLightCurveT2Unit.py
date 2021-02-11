73acf7f17348e981d7c793eebd30eaa0e2c4b641
vbrinnel <valery.brinnel@gmail.com>
2021-02-11 21:39:02 +0100

    DemoTiedLightCurveT2Unit: v0.7.1 compatibility update

--- ampel/demo/unit/base/DemoTiedLightCurveT2Unit.py
+++ ampel/demo/unit/base/DemoTiedLightCurveT2Unit.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# File              : Ampel-photometry/ampel/demo/unit/base/DemoLightCurveT2Unit.py
+# File              : Ampel-photometry/ampel/demo/unit/base/DemoTiedLightCurveT2Unit.py
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 07.08.2020
-# Last Modified Date: 07.08.2020
+# Last Modified Date: 11.02.2021
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
 from time import time
 from typing import Sequence, List
 from ampel.type import T2UnitResult
-from ampel.content.T2Record import T2Record
+from ampel.view.T2DocView import T2DocView
 from ampel.abstract.AbsTiedLightCurveT2Unit import AbsTiedLightCurveT2Unit
 from ampel.view.LightCurve import LightCurve
 
@@ -22,10 +22,11 @@ class DemoTiedLightCurveT2Unit(AbsTiedLightCurveT2Unit):
 	def get_tied_unit_names(self) -> List[str]:
 		return ["DemoPointT2Unit"]
 
-	def run(self, lightcurve: LightCurve, t2_records: Sequence[T2Record]) -> T2UnitResult:
+	def run(self, lightcurve: LightCurve, t2_views: Sequence[T2DocView]) -> T2UnitResult:
 		return {
-			"id": lightcurve.get_values("jd"),
+			"jds": lightcurve.get_values("jd"),
 			"time": time(),
-			"linked_records": t2_records,
+			"linked_records": [el.serialize() for el in t2_views],
+			"linked_results": [el.get_payload() for el in t2_views],
 			"test_parameter": self.test_parameter
 		}
