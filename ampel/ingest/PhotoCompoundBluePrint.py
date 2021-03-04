0d71e374d3fd5a23845349b6cf6ad207f604ed3d
vbrinnel <valery.brinnel@gmail.com>
2021-03-04 18:51:33 +0100

    PhotoCompoundBluePrint: no need for dataclass

--- ampel/ingest/PhotoCompoundBluePrint.py
+++ ampel/ingest/PhotoCompoundBluePrint.py
@@ -4,16 +4,14 @@
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 01.01.2018
-# Last Modified Date: 18.03.2020
+# Last Modified Date: 04.03.2021
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
 from typing import Set, Dict
-from dataclasses import dataclass, field
 from ampel.type import ChannelId, StrictIterable
 from ampel.ingest.CompoundBluePrint import CompoundBluePrint
 
 
-@dataclass
 class PhotoCompoundBluePrint(CompoundBluePrint):
 	"""
 	Instance members of this class are typically set/updated by PhotoCompoundBluePrintGenerator
@@ -22,10 +20,16 @@ class PhotoCompoundBluePrint(CompoundBluePrint):
 	"""
 
 	# save channels names using pp id as key
-	d_ppsid_chnames: Dict[bytes, Set[ChannelId]] = field(default_factory=dict)
+	d_ppsid_chnames: Dict[bytes, Set[ChannelId]]
 
 	# save eid <-> ppsid association
-	d_eid_ppsid: Dict[bytes, bytes] = field(default_factory=dict)
+	d_eid_ppsid: Dict[bytes, bytes]
+
+
+	def __init__(self):
+		super().__init__()
+		self.d_ppsid_chnames = {}
+		self.d_eid_ppsid = {}
 
 
 	def get_ppsids_of_chans(self, chan_names: StrictIterable[ChannelId]) -> Set[bytes]:
