3182105c6a8a6d8a018f6a1cd62df758ae4bbf27
vbrinnel <valery.brinnel@gmail.com>
2021-04-03 23:43:46 +0200

    Rename AbsPhotoT3Unit -> AbsPagedPhotoT3Unit

--- ampel/abstract/AbsPagedPhotoT3Unit.py
+++ ampel/abstract/AbsPagedPhotoT3Unit.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# File              : Ampel-photometry/ampel/abstract/AbsPhotoT3Unit.py
+# File              : Ampel-photometry/ampel/abstract/AbsPagedPhotoT3Unit.py
 # License           : BSD-3-Clause
 # Author            : vb <vbrinnel@physik.hu-berlin.de>
 # Date              : 01.06.2020
@@ -10,12 +10,12 @@
 from typing import Tuple, Optional, Union, Dict
 from ampel.type import StockId
 from ampel.base import abstractmethod
-from ampel.abstract.AbsT3Unit import AbsT3Unit
+from ampel.abstract.AbsPagedT3Unit import AbsPagedT3Unit
 from ampel.view.TransientView import TransientView
 from ampel.struct.JournalTweak import JournalTweak
 
 
-class AbsPhotoT3Unit(AbsT3Unit[TransientView], abstract=True):
+class AbsPagedPhotoT3Unit(AbsPagedT3Unit[TransientView], abstract=True):
 	"""
 	Parametrized abstract class for T3 units receiving TransientView instances
 	(and potentially LightCurve instances as well)
