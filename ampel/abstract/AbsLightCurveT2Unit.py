a3377e68bf54044a3218d04ea833c6eefdaf34a4
vbrinnel <valery.brinnel@gmail.com>
2021-07-23 15:32:21 +0200

    AbsLightCurveT2Unit: remove obsolete docstring

--- ampel/abstract/AbsLightCurveT2Unit.py
+++ ampel/abstract/AbsLightCurveT2Unit.py
@@ -7,7 +7,7 @@
 # Last Modified Date: 01.03.2020
 # Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
 
-from typing import Iterable, ClassVar, Dict, Any
+from typing import Iterable
 from ampel.view.LightCurve import LightCurve
 from ampel.content.T1Document import T1Document
 from ampel.content.DataPoint import DataPoint
@@ -18,16 +18,6 @@ class AbsLightCurveT2Unit(AbsCustomStateT2Unit[LightCurve], abstract=True):
 	"""
 	Base class for T2s that operate on light curves.
 	"""
-
-	#: Light curve compilation settings.
-	#: These are extracted in :meth:`AbsT2Ingester.add_ingest_models() <ampel.abstract.ingest.AbsT2Ingester.AbsT2Ingester.add_ingest_models>`, combined
-	#: with default settings from other sources, passed to :meth:`PhotoT2Compiler.set_ingest_options() <ampel.compile.PhotoT2Compiler.PhotoT2Compiler.set_ingest_options>`, and used in :meth:`PhotoT2Compiler.compile() <ampel.compile.PhotoT2Compiler.PhotoT2Compiler.compile>`.
-	#:
-	#: upper_limits:
-	#:   - True: compound id computed using photopoint & upperlimit ids
-	#:   - False: compound id computed using photopoint ids only
-	ingest: ClassVar[Dict[str, Any]] = {'upper_limits': True}
-
 
 	@staticmethod
 	def build(compound: T1Document, datapoints: Iterable[DataPoint]) -> LightCurve:
