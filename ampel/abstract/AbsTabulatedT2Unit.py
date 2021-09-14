8666334e4fb3f2df1531ee727466a0504a6a1978
Marcus Fenner <mfenner@physik.hu-berlin.de>
2021-09-14 19:26:13 +0200

    Add get_flux_table to tabulated T2s

--- ampel/abstract/AbsTabulatedT2Unit.py
+++ ampel/abstract/AbsTabulatedT2Unit.py
@@ -4,20 +4,22 @@
 # License           : BSD-3-Clause
 # Author            : mf <mf@physik.hu-berlin.de>
 # Date              : 08.08.2021
-# Last Modified Date: 02.09.2021
+# Last Modified Date: 13.09.2021
 # Last Modified By  : mf <mf@physik.hu-berlin.de>
 
-from typing import Iterable, List, ClassVar, Dict, Sequence, Any
+from typing import List, ClassVar, Dict, Sequence, Any, Union, Optional
 from ampel.view.LightCurve import LightCurve
 from ampel.content.T1Document import T1Document
 from ampel.content.DataPoint import DataPoint
-from ampel.abstract.AbsStateT2Unit import AbsStateT2Unit
+from ampel.base.AmpelABC import AmpelABC
+from ampel.base.LogicalUnit import LogicalUnit
 from ampel.base.AuxUnitRegister import AuxUnitRegister
 from ampel.abstract.AbsT2Tabulator import AbsT2Tabulator
 from ampel.model.UnitModel import UnitModel
 
+from astropy.table import vstack, Table
 
-class AbsTabulatedT2Unit(AbsStateT2Unit, abstract=True):
+class AbsTabulatedT2Unit(AmpelABC, LogicalUnit, abstract=True):
     """
     Base class for T2s that operate on tabulated data.
     """
@@ -30,3 +32,13 @@ class AbsTabulatedT2Unit(AbsStateT2Unit, abstract=True):
         super().__init__(**kwargs)
         self._tab_engines: Sequence[AbsT2Tabulator] = [AuxUnitRegister.new_unit(model = el, sub_type = AbsT2Tabulator) for el in self.tabulator]
 
+    def get_flux_table(self, dps: Union[LightCurve, List[DataPoint]], compound: Optional[T1Document] = None) -> Table:
+        tables = [tab.get_flux_table(dps, compound) for tab in self._tab_engines]
+        if len(tables) == 1:
+            table = tables[0]
+        elif len(tables) > 1:
+            table = vstack(tables)
+        else:
+            raise NotImplementedError
+        table.sort('time')
+        return table
