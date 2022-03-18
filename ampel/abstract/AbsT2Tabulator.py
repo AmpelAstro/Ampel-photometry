6059ad99785ae5ea76a5afd4790b00b2c872def8
Marcus Fenner <mfenner@physik.hu-berlin.de>
2022-03-18 14:11:27 +0100

    Update Tabulators

--- ampel/abstract/AbsT2Tabulator.py
+++ ampel/abstract/AbsT2Tabulator.py
@@ -2,30 +2,39 @@
 # -*- coding: utf-8 -*-
 # File              : Ampel-lsst/ampel/abstract/AbsT2Tabulator.py
 # License           : BSD-3-Clause
-# Author            : mf <mf@physik.hu-berlin.de>
+# Author            : Marcus Fenner <mf@physik.hu-berlin.de>
 # Date              : 25.05.2021
-# Last Modified Date: 13.09.2021
-# Last Modified By  : mf <mf@physik.hu-berlin.de>
+# Last Modified Date: 21.03.2022
+# Last Modified By  : Marcus Fenner <mf@physik.hu-berlin.de>
 
-from typing import Sequence, Optional, List, Any, Tuple, Union
-from ampel.base.AmpelBaseModel import AmpelBaseModel
+from typing import Any, List, Sequence, Tuple, Union
+
 from ampel.base.AmpelABC import AmpelABC
+from ampel.base.AmpelBaseModel import AmpelBaseModel
+from ampel.base.decorator import abstractmethod
 from ampel.content.DataPoint import DataPoint
 from astropy.table import Table
-from ampel.view.LightCurve import LightCurve
-from ampel.base.decorator import abstractmethod
-from ampel.content.T1Document import T1Document
+
 
 class AbsT2Tabulator(AmpelABC, AmpelBaseModel, abstract=True):
     """ """
+
     @abstractmethod
-    def get_flux_table(self, dps: Union[LightCurve, List[DataPoint]], compound: Optional[T1Document] = None) -> Table:
+    def get_flux_table(self, dps: List[DataPoint]) -> Table:
         ...
 
     @abstractmethod
-    def get_pos(self, dps: Union[LightCurve, List[DataPoint]], compound: Optional[T1Document] = None) -> Sequence[Tuple[Any, Any]]:
+    def get_pos(self, dps: List[DataPoint]) -> Sequence[Tuple[Any, Any]]:
         ...
 
     @abstractmethod
-    def get_jd(self, dps: Union[LightCurve, List[DataPoint]], compound: Optional[T1Document] = None) -> Sequence[Any]:
+    def get_jd(self, dps: List[DataPoint]) -> Sequence[Any]:
+        ...
+
+    @abstractmethod
+    def get_stock_id(self, dps: List[DataPoint]) -> set[int]:
+        ...
+
+    @abstractmethod
+    def get_stock_name(self, dps: List[DataPoint]) -> list[str]:
         ...
--- ampel/abstract/AbsTabulatedT2Unit.py
+++ ampel/abstract/AbsTabulatedT2Unit.py
@@ -2,22 +2,23 @@
 # -*- coding: utf-8 -*-
 # File              : Ampel-lsst/ampel/abstract/AbsTabulatedT2Unit.py
 # License           : BSD-3-Clause
-# Author            : mf <mf@physik.hu-berlin.de>
+# Author            : Marcus Fenner <mf@physik.hu-berlin.de>
 # Date              : 08.08.2021
-# Last Modified Date: 13.09.2021
-# Last Modified By  : mf <mf@physik.hu-berlin.de>
+# Last Modified Date: 17.03.2022
+# Last Modified By  : Marcus Fenner <mf@physik.hu-berlin.de>
 
-from typing import List, ClassVar, Dict, Sequence, Any, Union, Optional
-from ampel.view.LightCurve import LightCurve
-from ampel.content.T1Document import T1Document
-from ampel.content.DataPoint import DataPoint
+from typing import Any, ClassVar, Dict, List, Optional, Sequence, Union
+
 from ampel.base.AmpelABC import AmpelABC
-from ampel.base.LogicalUnit import LogicalUnit
 from ampel.base.AuxUnitRegister import AuxUnitRegister
-from ampel.abstract.AbsT2Tabulator import AbsT2Tabulator
+from ampel.base.LogicalUnit import LogicalUnit
+from ampel.content.DataPoint import DataPoint
 from ampel.model.UnitModel import UnitModel
+from astropy.table import Table, vstack
 
-from astropy.table import vstack, Table
+from ampel.abstract.AbsT2Tabulator import AbsT2Tabulator
+import math
+
 
 class AbsTabulatedT2Unit(AmpelABC, LogicalUnit, abstract=True):
     """
@@ -27,18 +28,52 @@ class AbsTabulatedT2Unit(AmpelABC, LogicalUnit, abstract=True):
     ingest: ClassVar[Dict[str, Any]]
     tabulator: Sequence[UnitModel] = []
 
-    def __init__(self, **kwargs):
-        self.tabulator = kwargs['tabulator']
+    def __init__(self, **kwargs) -> None:
+        self.tabulator = kwargs["tabulator"]
         super().__init__(**kwargs)
-        self._tab_engines: Sequence[AbsT2Tabulator] = [AuxUnitRegister.new_unit(model = el, sub_type = AbsT2Tabulator) for el in self.tabulator]
+        self._tab_engines: Sequence[AbsT2Tabulator] = [
+            AuxUnitRegister.new_unit(model=el, sub_type=AbsT2Tabulator)
+            for el in self.tabulator
+        ]
 
-    def get_flux_table(self, dps: Union[LightCurve, List[DataPoint]], compound: Optional[T1Document] = None) -> Table:
-        tables = [tab.get_flux_table(dps, compound) for tab in self._tab_engines]
+    def get_flux_table(
+        self,
+        dps: List[DataPoint],
+        jd_start: Optional[float] = None,
+        jd_end: Optional[float] = None,
+    ) -> Table:
+        tables = [tab.get_flux_table(dps) for tab in self._tab_engines]
         if len(tables) == 1:
             table = tables[0]
         elif len(tables) > 1:
-            table = vstack(tables)
+            table = vstack(tables, join_type="exact")
         else:
             raise NotImplementedError
-        table.sort('time')
+        table.sort("time")
+        if jd_start and not math.isinf(jd_start):
+            mask = table["time"] < jd_start
+            table = table[~mask]
+        if jd_end and not math.isinf(jd_end):
+            mask = table["time"] < jd_end
+            table = table[~mask]
         return table
+
+    def get_stock_id(
+        self,
+        dps: List[DataPoint],
+    ) -> List[int]:
+        return [
+            stock
+            for tab in self._tab_engines
+            for stock in tab.get_stock_id(dps)
+        ]
+
+    def get_stock_name(
+        self,
+        dps: List[DataPoint],
+    ) -> List[Union[str, int]]:
+        return [
+            name
+            for tab in self._tab_engines
+            for name in tab.get_stock_name(dps)
+        ]
